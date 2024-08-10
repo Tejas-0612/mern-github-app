2c275a5165d57458b23ade7b0c99c119e063d583
Tejas <tejashinde0612@gmail.com>
2024-08-10 09:38:37 +0530

    changes in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import passport from "passport";
+import session from "express-session";
 
 import "./passport/github.auth.js";
 import userRoute from "./routes/user.route.js";
 import exploreRoute from "./routes/explore.route.js";
 import authRoute from "./routes/auth.route.js";
 import connectDB from "./db/index.js";
-import passport from "passport";
-import session from "express-session";
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 const corsOptions = {
   origin: "http://localhost:3000",
-  credentials: true, // This enables credentials in CORS
+  credentials: true,
 };
 dotenv.config();
 
 const app = express();
 
 // no need to add cors as frontend & backend both are on same domain
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
